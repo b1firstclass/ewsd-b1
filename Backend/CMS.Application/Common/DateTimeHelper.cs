7317039d0c6e0f0f42ccb095b75a63d6f35f0e8d
Min-Khant <m.khant612000@gmail.com>
2026-02-20 20:20:20 +0630

    Chore: enhance UTC datetime handling

---
+++ Backend/CMS.Application/Common/DateTimeHelper.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace CMS.Application.Common
+{
+    public static class DateTimeHelper
+    {
+        public static DateTime NormalizeToUtc(DateTime dateTime)
+        {
+            return dateTime.Kind switch
+            {
+                DateTimeKind.Utc => dateTime,
+                DateTimeKind.Local => dateTime.ToUniversalTime(),
+                _ => DateTime.SpecifyKind(dateTime, DateTimeKind.Utc)
+            };
+        }
+
+        public static DateTime? NormalizeToUtc(DateTime? dateTime)
+        {
+            return dateTime.HasValue ? NormalizeToUtc(dateTime.Value) : null;
+        }
+    }
+}
--- Backend/CMS.Infrastructure/Mappings/DomainMappingProfile.cs
+++ Backend/CMS.Infrastructure/Mappings/DomainMappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using CMS.Application.Common;
 using CMS.Application.DTOs;
 using CMS.Domain.Entities;
 using System;
@@ -23,8 +24,8 @@ namespace CMS.Infrastructure.Mappings
                 .ForMember(dest => dest.LoginId, opt => opt.MapFrom(src => src.LoginId))
                 .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FullName))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => src.CreatedDate))
-                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(src => src.ModifiedDate))
+                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => DateTimeHelper.NormalizeToUtc(src.CreatedDate)))
+                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(src => DateTimeHelper.NormalizeToUtc(src.ModifiedDate)))
                 .ForMember(dest => dest.Faculties, opt => opt.MapFrom(src => src.Faculties))
                 .ForMember(dest => dest.Roles, opt => opt.MapFrom(src => src.Roles));
 
@@ -36,7 +37,7 @@ namespace CMS.Infrastructure.Mappings
             CreateMap<Faculty, FaculityInfo>()
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.FacultyName))
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.FacultyId))
-                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => src.CreatedDate));
+                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => DateTimeHelper.NormalizeToUtc(src.CreatedDate)));
 
             CreateMap<RoleCreateRequest, Role>()
                 .ForMember(dest => dest.RoleId, opt => opt.MapFrom(_ => Guid.NewGuid().ToString()))
@@ -48,8 +49,8 @@ namespace CMS.Infrastructure.Mappings
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.RoleId))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
-                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => src.CreatedDate))
-                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(src => src.ModifiedDate))
+                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => DateTimeHelper.NormalizeToUtc(src.CreatedDate)))
+                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(src => DateTimeHelper.NormalizeToUtc(src.ModifiedDate)))
                 .ForMember(dest => dest.Permissions, opt => opt.MapFrom(src => src.Permissions));
 
             CreateMap<PermissionCreateRequest, Permission>()
@@ -64,8 +65,8 @@ namespace CMS.Infrastructure.Mappings
                 .ForMember(dest => dest.Module, opt => opt.MapFrom(src => src.Module))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
-                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => src.CreatedDate))
-                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(src => src.ModifiedDate));
+                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => DateTimeHelper.NormalizeToUtc(src.CreatedDate)))
+                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(src => DateTimeHelper.NormalizeToUtc(src.ModifiedDate)));
 
             CreateMap<ContributionWindowCreateRequest, ContributionWindow>()
                 .ForMember(dest => dest.ContributionWindowId, opt => opt.MapFrom(_ => Guid.NewGuid()))
@@ -73,14 +74,14 @@ namespace CMS.Infrastructure.Mappings
 
             CreateMap<ContributionWindow, ContributionWindowInfo>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ContributionWindowId))
-                .ForMember(dest => dest.SubmissionOpenDate, opt => opt.MapFrom(src => src.SubmissionOpenDate.ToUniversalTime()))
-                .ForMember(dest => dest.SubmissionEndDate, opt => opt.MapFrom(src => src.SubmissionEndDate.ToUniversalTime()))
-                .ForMember(dest => dest.ClosureDate, opt => opt.MapFrom(src => src.ClosureDate.ToUniversalTime()))
+                .ForMember(dest => dest.SubmissionOpenDate, opt => opt.MapFrom(src => DateTimeHelper.NormalizeToUtc(src.SubmissionOpenDate)))
+                .ForMember(dest => dest.SubmissionEndDate, opt => opt.MapFrom(src => DateTimeHelper.NormalizeToUtc(src.SubmissionEndDate)))
+                .ForMember(dest => dest.ClosureDate, opt => opt.MapFrom(src => DateTimeHelper.NormalizeToUtc(src.ClosureDate)))
                 .ForMember(dest => dest.AcademicYearStart, opt => opt.MapFrom(src => src.AcademicYearStart))
                 .ForMember(dest => dest.AcademicYearEnd, opt => opt.MapFrom(src => src.AcademicYearEnd))
                 .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive))
-                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => src.CreatedDate))
-                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(src => src.ModifiedDate));
+                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => DateTimeHelper.NormalizeToUtc(src.CreatedDate)))
+                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(src => DateTimeHelper.NormalizeToUtc(src.ModifiedDate)));
         }
     }
 }
