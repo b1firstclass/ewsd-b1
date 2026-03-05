9e87caafccc071b973af8ae6e64ca5b2baaf7600
Thet Wai Aung <126937793+thetwaiaung-dev@users.noreply.github.com>
2026-03-05 15:03:14 +0630

    feat : implement faculty api setup

--- Frontend/src/types/constantApiRoute.ts
+++ Frontend/src/types/constantApiRoute.ts
@@ -1,8 +1,16 @@
 export const ApiRoute = {
     Auth: {
-        Login: "/users/login"
+        Login: "/users/login",
+        RefreshToken: "/users/refresh-token"
     },
     User: {
         profile: "/users/profile"
+    },
+    Faculity: {
+        List: "/Faculties",
+        GetById: (id: string) => `/Faculties/${id}`,
+        Create: "/Faculties",
+        Update: (id: string) => `/Faculties/${id}`,
+        Delete: (id: string) => `/Faculties/${id}`,
     }
 }
--- Frontend/src/types/faculityType.ts
+++ Frontend/src/types/faculityType.ts
@@ -1,6 +1,18 @@
-export interface Faculity{
+export interface Faculity {
     id: string;
     name: string;
     isActive: boolean;
     createdDate: string;
+}
+
+export interface FaculityCreateRequest {
+    name: string;
+}
+
+export interface FaculityUpdateRequest extends FaculityCreateRequest {
+    isActive: boolean;
+}
+
+export interface FaculityListResponse {
+    items: Faculity[];
 }
